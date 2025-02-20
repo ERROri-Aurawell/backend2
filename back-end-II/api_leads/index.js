1ead3c5df386e1cc3afde9877c4f28c3aa0dff94
ERROri-Aurawell <viniciusavila4080@gmail.com>
2025-02-20 17:42:14 -0400

    a

--- back-end-II/api_leads/index.js
+++ back-end-II/api_leads/index.js
@@ -1,13 +1,31 @@
 import express from 'express';
 import { cadastro } from './servico/cadastro_servico.js';
-import { validação } from './servico/validacao/valida.js';
+import { validaUsuario } from './servico/validacao/valida.js';
 
 
 
 app.post('/usuarios', async (req, res) => {
-    const { nome, email, telefone } = req.params;
+    const { nome, email, telefone } = req.body;
 
-    const resultado = await validação(nome, email, telefone);
+    const resultado = validaUsuario(nome, email, telefone);
     
-    res.status(204).end();
+    if (resultado.status) {
+        await cadastro(nome, email, telefone)
+        res.status(202).end();
+    } else {
+        res.status(400).json(resultado);
+    }
+
+
 })
+
+
+
+app.listen(9000, () => {
+    const data = new Date();
+    console.log("Servidor node iniciado em: " + data);
+
+    // const conexao = await pool.getConnection();
+    // console.log(conexao.threadId);  
+    // conexao.release();
+});
